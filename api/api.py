171fa7265175e73daf37de1a2ce3007b7fefad9a
devis92 <iqbol.adahamjonov@gmail.com>
2021-04-08 19:38:03 +0500

    router config

--- api/api.py
+++ api/api.py
@@ -1,6 +1,24 @@
 import time
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, static_folder="../../../../../JS/React/GitHub/react-projects/15-cocktails/setup/build", static_url_path='/')
+
+
+
+
+# emulate the root url
+@app.route('/')
+def index():
+    return app.send_static_file("index.html")
+
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+@app.route('/hello')
+def hello():
+    return "hello world"
 
 
 @app.route('/api/time')
